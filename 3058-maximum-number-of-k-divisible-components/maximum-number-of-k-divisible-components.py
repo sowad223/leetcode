5e7e49854f1d57eff21414ae7adc7680e4e5cbf7
Sowad Rahman <85169265+sowad223@users.noreply.github.com>
2024-12-21 13:58:23 +0600

    Time: 185 ms (45.12%) | Memory: 31.9 MB (89.02%) - LeetSync

--- 3058-maximum-number-of-k-divisible-components/maximum-number-of-k-divisible-components.py
+++ 3058-maximum-number-of-k-divisible-components/maximum-number-of-k-divisible-components.py
@@ -5,26 +5,30 @@ class Solution:
             tree[a].append(b)
             tree[b].append(a)
         
-        
         max_components = 0
-
+        visited = [False] * n
+        stack = [(0, -1)]
+        subtree_sums = [0] * n
         
-        def dfs(node, parent):
-            nonlocal max_components
-            subtree_sum = values[node]  
-
-            for neighbor in tree[node]:
-                if neighbor != parent:
-                    child_sum = dfs(neighbor, node)
-                    subtree_sum += child_sum
-
+        while stack:
+            node, parent = stack.pop()
             
-            if subtree_sum % k == 0:
-                max_components += 1
-                return 0  
-            return subtree_sum
-
-       
-        dfs(0, -1)
+            if not visited[node]:
+                visited[node] = True
+                stack.append((node, parent))
+                for neighbor in tree[node]:
+                    if neighbor != parent and not visited[neighbor]:
+                        stack.append((neighbor, node))
+            else:
+                subtree_sum = values[node]
+                for neighbor in tree[node]:
+                    if neighbor != parent:
+                        subtree_sum += subtree_sums[neighbor]
+                
+                if subtree_sum % k == 0:
+                    max_components += 1
+                    subtree_sum = 0
+                
+                subtree_sums[node] = subtree_sum
 
         return max_components

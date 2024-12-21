dcf73d0f6f28047c2bd02ba58202669a7433d056
Sowad Rahman <85169265+sowad223@users.noreply.github.com>
2024-12-21 13:55:48 +0600

    Time: 123 ms (71.95%) | Memory: 41.2 MB (9.76%) - LeetSync

---
+++ 3058-maximum-number-of-k-divisible-components/maximum-number-of-k-divisible-components.py
@@ -0,0 +1,30 @@
+class Solution:
+    def maxKDivisibleComponents(self, n: int, edges: List[List[int]], values: List[int], k: int) -> int:
+        tree = defaultdict(list)
+        for a, b in edges:
+            tree[a].append(b)
+            tree[b].append(a)
+        
+        
+        max_components = 0
+
+        
+        def dfs(node, parent):
+            nonlocal max_components
+            subtree_sum = values[node]  
+
+            for neighbor in tree[node]:
+                if neighbor != parent:
+                    child_sum = dfs(neighbor, node)
+                    subtree_sum += child_sum
+
+            
+            if subtree_sum % k == 0:
+                max_components += 1
+                return 0  
+            return subtree_sum
+
+       
+        dfs(0, -1)
+
+        return max_components

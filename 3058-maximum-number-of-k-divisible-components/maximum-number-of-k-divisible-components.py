3fdc224a5f6911f6b3a8008537100a8c887963d2
Sowad Rahman <85169265+sowad223@users.noreply.github.com>
2024-12-21 14:01:36 +0600

    Time: 136 ms (64.63%) | Memory: 41.2 MB (9.76%) - LeetSync

--- 3058-maximum-number-of-k-divisible-components/maximum-number-of-k-divisible-components.py
+++ 3058-maximum-number-of-k-divisible-components/maximum-number-of-k-divisible-components.py
@@ -5,25 +5,27 @@ class Solution:
             tree[a].append(b)
             tree[b].append(a)
         
+        
         max_components = 0
 
-        # Sorting the adjacency list to ensure sorted traversal order (artificial O(n log n) addition)
-        for node in tree:
-            tree[node].sort()
-
+        
         def dfs(node, parent):
             nonlocal max_components
-            subtree_sum = values[node]
+            subtree_sum = values[node]  
 
             for neighbor in tree[node]:
                 if neighbor != parent:
-                    subtree_sum += dfs(neighbor, node)
+                    child_sum = dfs(neighbor, node)
+                    subtree_sum += child_sum
 
+            
             if subtree_sum % k == 0:
                 max_components += 1
-                return 0
-
+                return 0  
             return subtree_sum
 
+       
         dfs(0, -1)
-        return max_components
+
+        return max_components 
+

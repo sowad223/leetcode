8240787a37cacf80dcd0bbd4cd42ad9d1b6fd509
Sowad Rahman <85169265+sowad223@users.noreply.github.com>
2025-01-07 12:14:12 +0600

    Time: 4 ms (31.92%) | Memory: 11.3 MB (60.52%) - LeetSync

--- 1524-string-matching-in-an-array/string-matching-in-an-array.cpp
+++ 1524-string-matching-in-an-array/string-matching-in-an-array.cpp
@@ -1,27 +1,19 @@
-#include <vector>
-#include <string>
-#include <algorithm>
-
 class Solution {
 public:
-    std::vector<std::string> stringMatching(std::vector<std::string>& words) {
-        std::vector<std::string> result;
-        
-        
-        std::sort(words.begin(), words.end(), [](const std::string& a, const std::string& b) {
-            return a.size() < b.size();
-        });
-        
-        
-        for (int i = 0; i < words.size(); ++i) {
-            for (int j = i + 1; j < words.size(); ++j) {
-                if (words[j].find(words[i]) != std::string::npos) { 
-                    result.push_back(words[i]);
-                    break; 
-                }
+    vector<string> stringMatching(vector<string>& words) {
+        vector<string> str;
+
+        for (int i = 0; i < words.size(); i++) {
+            for (int j = 0; j < words.size(); j++) {
+               if (i != j) {
+                   auto found = words[j].find(words[i]);
+                   if (found != string::npos) {
+                       str.push_back(words[i]);
+                       break;
+                   }
+               }
             }
         }
-        
-        return result;
+        return str;
     }
 };

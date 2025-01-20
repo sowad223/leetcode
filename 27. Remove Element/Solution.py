165f7751f5bcaf9e972074ba5427bb7edcf1feaa
Sowad Rahman <85169265+sowad223@users.noreply.github.com>
2025-01-20 08:50:28 +0600

    Python Solution
    Submission Statistics:
    Question #: 27
    Language: Python
    Runtime: 0 ms
    Runtime percentile: 100.00
    Memory: 12.496 MB
    Memory percentile: 29.70
    
    Question Title: Remove Element
    
    Question Body: Can you solve this real interview question? Remove Element - Given an integer array nums and an integer val, remove all occurrences of val in nums in-place [https://en.wikipedia.org/wiki/In-place_algorithm]. The order of the elements may be changed. Then return the number of elements in nums which are not equal to val.
    
    Consider the number of elements in nums which are not equal to val be k, to get accepted, you need to do the following things:
    
     * Change the array nums such that the first k elements of nums contain the elements which are not equal to val. The remaining elements of nums are not important as well as the size of nums.
     * Return k.
    
    Custom Judge:
    
    The judge will test your solution with the following code:
    
    
    int[] nums = [...]; // Input array
    int val = ...; // Value to remove
    int[] expectedNums = [...]; // The expected answer with correct length.
                                // It is sorted with no values equaling val.
    
    int k = removeElement(nums, val); // Calls your implementation
    
    assert k == expectedNums.length;
    sort(nums, 0, k); // Sort the first k elements of nums
    for (int i = 0; i &lt; actualLength; i++) {
        assert nums[i] == expectedNums[i];
    }
    
    
    If all assertions pass, then your solution will be accepted.
    
     
    
    Example 1:
    
    
    Input: nums = [3,2,2,3], val = 3
    Output: 2, nums = [2,2,_,_]
    Explanation: Your function should return k = 2, with the first two elements of nums being 2.
    It does not matter what you leave beyond the returned k (hence they are underscores).
    
    
    Example 2:
    
    
    Input: nums = [0,1,2,2,3,0,4,2], val = 2
    Output: 5, nums = [0,1,4,0,3,_,_,_]
    Explanation: Your function should return k = 5, with the first five elements of nums containing 0, 0, 1, 3, and 4.
    Note that the five elements can be returned in any order.
    It does not matter what you leave beyond the returned k (hence they are underscores).
    
    
     
    
    Constraints:
    
     * 0 &lt;= nums.length &lt;= 100
     * 0 &lt;= nums[i] &lt;= 50
     * 0 &lt;= val &lt;= 100

---
+++ 27. Remove Element/Solution.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def removeElement(self, nums, val):
+        """
+        :type nums: List[int]
+        :type val: int
+        :rtype: int
+        """
+        n=len(nums)
+
+        
+  
+        j = 0
+        for i in range(n):
+            if nums[i] != val:
+                nums[j] = nums[i]
+                j += 1
+        
+
+        return j
+        

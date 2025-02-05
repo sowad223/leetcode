6d18ed38c888a03b8da9f2ca106a74ff105c054f
Sowad Rahman <sowadrahman3@gmail.com>
2025-02-05 09:31:01 +0600

    Time: 0 ms (100.00%) | Memory: 17.7 MB (67.41%) - LeetSync

---
+++ 1915-check-if-one-string-swap-can-make-strings-equal/check-if-one-string-swap-can-make-strings-equal.py
@@ -0,0 +1,19 @@
+from sortedcontainers import SortedList
+
+class Solution:
+    def areAlmostEqual(self, s1: str, s2: str) -> bool:
+        if s1 == s2:
+            return True
+        
+        mismatch = SortedList()  
+        
+        for i in range(len(s1)): 
+            if s1[i] != s2[i]:
+                mismatch.add(i)
+        
+        if len(mismatch) != 2:
+            return False
+        
+        i, j = mismatch
+        return s1[i] == s2[j] and s1[j] == s2[i] 
+
